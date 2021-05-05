da2ac17683ec27c66bd8d4f156a19a48d988755e
diego-primo <diego.pg.dias@gmail.com>
2021-05-05 14:39:25 -0300

    Acréscimo das classes Order() e OrderStatus() e praticando enumeração do java.

--- src/curso_java/Programa.java
+++ src/curso_java/Programa.java
@@ -7,11 +7,16 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Programa {
 
 	public static void main(String[] args) throws Exception {
-		bomDia();
-		boaTarde();
+
+		Order order = new Order(1080, new Date(), OrderStatus.PAGAMENTOPENDENTE);
+
+		System.out.println(order);
 
 	}
 
@@ -140,24 +145,23 @@ public class Programa {
 
 		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
 		System.out.println(sdf.format(d));
-				
+
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(d);
 		cal.add(Calendar.DAY_OF_MONTH, 2);
 		d = cal.getTime();
-		
-		System.out.println(sdf.format(d));
 
+		System.out.println(sdf.format(d));
 
 //		int minutes = cal.get(Calendar.MINUTE);
 //		int month = 1 + cal.get(Calendar.MONTH);
 //		System.out.println("Minutes: " + minutes);
 //		System.out.println("Month: " + month);
 	}
-	
+
 	public static void bomDia() {
 		System.out.println("Bom dia!");
-		
+
 		Date dt = new Date();
 		System.out.println(dt);
 		System.out.println("######################################################");
@@ -165,7 +169,7 @@ public class Programa {
 		System.out.println(sdf2.format(dt));
 
 	}
-	
+
 	public static void boaTarde() {
 		System.out.println("Boa Tarde");
 		Date dt = new Date();
@@ -173,10 +177,8 @@ public class Programa {
 		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		System.out.println(sdf2.format(dt));
 
-		
 		System.out.println("######################################################");
 
-		
 		System.out.println("Acréscimo de 2 dias:");
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(dt);
---
+++ src/entities/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package entities.enums;
+
+public enum OrderStatus {
+	
+	PAGAMENTOPENDENTE,
+	PROCESSANDO,
+	ENVIADO,
+	ENTREGUE;
+	
+
+}
