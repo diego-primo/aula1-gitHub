7e64768e983beffdacca35b71cd9b36651e9983c
diego-primo <diego.pg.dias@gmail.com>
2021-05-06 15:11:16 -0300

    crtl+shift+f e depois salve

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -6,16 +6,14 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
+
 		try {
-			String [] vect = sc.nextLine().split(" ");
+			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
 			System.out.println(vect[position]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Posição inválida");
-		}catch (InputMismatchException e) {
+		} catch (InputMismatchException e) {
 			System.out.println("Erro de entrada");
 		}
 		System.out.println("Fim do programa");
